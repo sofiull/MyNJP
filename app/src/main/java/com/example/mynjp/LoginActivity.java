3805e8f72c80d6a77cb6d1387bc56cee80ffcc22
sofiull <sofiulfr@gmail.com>
2019-10-06 12:38:15 +0700

    Update minor tampilan fragments dan mempersiapkan tampilan action bar

--- app/src/main/java/com/example/mynjp/LoginActivity.java
+++ app/src/main/java/com/example/mynjp/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
@@ -13,6 +14,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu,menu);
+//        return true;
+//    }
 
     public void submit(View view) {
         Intent intent = new Intent(this,MainActivity.class);
