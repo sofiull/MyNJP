5c803ed13a35e391d3e3b3dc40d30253301f62ec
path71nder <sevillebayuaji@gmail.com>
2019-11-27 16:55:43 +0700

    Mengubah tampilan login menggunakan Android UI Firebase

--- app/src/main/java/com/example/mynjp/LoginActivity.java
+++ app/src/main/java/com/example/mynjp/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.mynjp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,20 +13,30 @@ import android.widget.Toast;
 import com.example.mynjp.fragments.LoginFragment;
 import com.example.mynjp.fragments.RegisterFragment;
 import com.example.mynjp.model.User;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText namainput;
     private EditText alamatinput;
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        namainput = findViewById(R.id.input_email);
-        alamatinput = findViewById(R.id.input_password);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, new LoginFragment())
-                .commit();
+        //namainput = findViewById(R.id.input_email);
+        //alamatinput = findViewById(R.id.input_password);
+        //getSupportFragmentManager().beginTransaction()
+          //      .replace(R.id.fragment_container, new LoginFragment())
+            //    .commit();
     }
 
 //    @Override
@@ -33,6 +44,44 @@ public class LoginActivity extends AppCompatActivity {
 //        getMenuInflater().inflate(R.menu.menu,menu);
 //        return true;
 //    }
+
+    public void createSignInIntent(){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+    public void signOut(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                });
+    }
 
     public void submit(View view) {
         String nama = namainput.getText().toString();
