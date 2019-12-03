6c7b0ea8f1ce5159b94bc8e359c1c10d6e643122
path71nder <sevillebayuaji@gmail.com>
2019-12-03 19:31:45 +0700

    Memperbaiki Login

--- app/src/main/java/com/example/mynjp/LoginActivity.java
+++ app/src/main/java/com/example/mynjp/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -32,18 +33,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //namainput = findViewById(R.id.input_email);
-        //alamatinput = findViewById(R.id.input_password);
-        //getSupportFragmentManager().beginTransaction()
-          //      .replace(R.id.fragment_container, new LoginFragment())
-            //    .commit();
+        namainput = findViewById(R.id.input_email);
+        alamatinput = findViewById(R.id.input_password);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new LoginFragment())
+                .commit();
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu,menu);
-//        return true;
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
 
     public void createSignInIntent(){
         List<AuthUI.IdpConfig> providers = Arrays.asList(
